e9c6beb449ad9aeba52ea1ab627114d552588c7e
KAI <jhongkaihuang@gmail.com>
2022-10-20 00:49:20 +0800

    update

--- client/main.lua
+++ client/main.lua
@@ -1,59 +1,11 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local PlayerData = QBCore.Functions.GetPlayerData()
-local isLoggedIn = LocalPlayer.state.isLoggedIn
 local playerPed = PlayerPedId()
 local playerCoords = GetEntityCoords(playerPed)
-local closestCityhall = nil
-local closestDrivingSchool = nil
-local inCityhallPage = false
-local inRangeCityhall = false
-local inRangeDrivingSchool = false
 local pedsSpawned = false
-local table_clone = table.clone
 local blips = {}
 
 -- Functions
-
-local function getClosestHall()
-    local distance = #(playerCoords - Config.Cityhalls[1].coords)
-    local closest = 1
-    for i = 1, #Config.Cityhalls do
-        local hall = Config.Cityhalls[i]
-        local dist = #(playerCoords - hall.coords)
-        if dist < distance then
-            distance = dist
-            closest = i
-        end
-    end
-    return closest
-end
-
-local function getClosestSchool()
-    local distance = #(playerCoords - Config.DrivingSchools[1].coords)
-    local closest = 1
-    for i = 1, #Config.DrivingSchools do
-        local school = Config.DrivingSchools[i]
-        local dist = #(playerCoords - school.coords)
-        if dist < distance then
-            distance = dist
-            closest = i
-        end
-    end
-    return closest
-end
-
-local function setCityhallPageState(bool, message)
-    if message then
-        local action = bool and "open" or "close"
-        SendNUIMessage({
-            action = action
-        })
-    end
-    SetNuiFocus(bool, bool)
-    inCityhallPage = bool
-    if not Config.UseTarget or bool then return end
-    inRangeCityhall = false
-end
 
 local function createBlip(options)
     if not options.coords or type(options.coords) ~= 'table' and type(options.coords) ~= 'vector3' then return error(('createBlip() expected coords in a vector3 or table but received %s'):format(options.coords)) end
@@ -81,33 +33,17 @@ local function deleteBlips()
 end
 
 local function initBlips()
-    for i = 1, #Config.Cityhalls do
-        local hall = Config.Cityhalls[i]
-        if hall.showBlip then
-            blips[#blips+1] = createBlip({
-                coords = hall.coords,
-                sprite = hall.blipData.sprite,
-                display = hall.blipData.display,
-                scale = hall.blipData.scale,
-                colour = hall.blipData.colour,
-                shortRange = true,
-                title = hall.blipData.title
-            })
-        end
-    end
-    for i = 1, #Config.DrivingSchools do
-        local school = Config.DrivingSchools[i]
-        if school.showBlip then
-            blips[#blips+1] = createBlip({
-                coords = school.coords,
-                sprite = school.blipData.sprite,
-                display = school.blipData.display,
-                scale = school.blipData.scale,
-                colour = school.blipData.colour,
-                shortRange = true,
-                title = school.blipData.title
-            })
-        end
+    local hall = Config.Cityhalls
+    if hall.showBlip then
+        blips[#blips+1] = createBlip({
+            coords = hall.coords,
+            sprite = hall.blipData.sprite,
+            display = hall.blipData.display,
+            scale = hall.blipData.scale,
+            colour = hall.blipData.colour,
+            shortRange = true,
+            title = hall.blipData.title
+        })
     end
 end
 
@@ -127,62 +63,11 @@ local function spawnPeds()
         TaskStartScenarioInPlace(ped, current.scenario, true, true)
         current.pedHandle = ped
         if Config.UseTarget then
-            local opts = nil
-            if current.drivingschool then
-                opts = {
-                    label = 'Take Driving Lessons',
-                    icon = 'fa-solid fa-car-side',
-                    action = function()
-                        TriggerServerEvent('qb-cityhall:server:sendDriverTest', Config.DrivingSchools[closestDrivingSchool].instructors)
-                    end
-                }
-            elseif current.cityhall then
-                opts = {
-                    label = 'Open Cityhall',
-                    icon = 'fa-solid fa-city',
-                    action = function()
-                        inRangeCityhall = true
-                        setCityhallPageState(true, true)
-                    end
-                }
-            end
-            if opts then
-                exports['qb-target']:AddTargetEntity(ped, {
-                    options = {opts},
-                    distance = 2.0
-                })
-            end
-        else
-            local options = current.zoneOptions
-            if options then
-                local zone = BoxZone:Create(current.coords.xyz, options.length, options.width, {
-                    name = "zone_cityhall_"..ped,
-                    heading = current.coords.w,
-                    debugPoly = false,
-                    minZ = current.coords.z - 3.0,
-                    maxZ = current.coords.z + 2.0
-                })
-                zone:onPlayerInOut(function(inside)
-                    if isLoggedIn and closestCityhall and closestDrivingSchool then
-                        if inside then
-                            if current.drivingschool then
-                                inRangeDrivingSchool = true
-                                exports['ps-ui']:DisplayText('[E] Take Driving Lessons', 'primary')
-                            elseif current.cityhall then
-                                inRangeCityhall = true
-                                exports['ps-ui']:DisplayText('[E] Open Cityhall', 'primary')
-                            end
-                        else
-                            exports['ps-ui']:HideText()
-                            if current.drivingschool then
-                                inRangeDrivingSchool = false
-                            elseif current.cityhall then
-                                inRangeCityhall = false
-                            end
-                        end
-                    end
-                end)
-            end
+            local opts = { label = Lang:t("info.menu"), icon = 'fa-solid fa-city', event = 'qb-cityhall:client:RegistrationDocs', }
+            exports['qb-target']:AddTargetEntity(ped, {
+                options = {opts},
+                distance = 2.0
+            })
         end
     end
     pedsSpawned = true
@@ -200,39 +85,31 @@ end
 
 -- Events
 
+RegisterNetEvent('qb-cityhall:client:RegistrationDocs', function()
+    local Menu = {}
+    Menu[#Menu + 1] = { isMenuHeader = true, header =  Lang:t('info.menu'), txt = "", }
+    for k,v in pairs(Config.Cityhalls.licenses) do
+        Menu[#Menu + 1] = { header = v.label, txt = '申請費: $'..v.cost, params = { event = 'qb-cityhall:client:requestId', args = { item = k } }}
+    end
+    exports['qb-menu']:openMenu(Menu)
+end)
+
+RegisterNetEvent('qb-cityhall:client:requestId', function(data)
+    TriggerServerEvent('qb-cityhall:server:requestId', data.item)
+end)
+
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     PlayerData = QBCore.Functions.GetPlayerData()
-    isLoggedIn = true
     spawnPeds()
 end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     PlayerData = {}
-    isLoggedIn = false
     deletePeds()
 end)
 
 RegisterNetEvent('QBCore:Player:SetPlayerData', function(val)
     PlayerData = val
-end)
-
-RegisterNetEvent('qb-cityhall:client:getIds', function()
-    TriggerServerEvent('qb-cityhall:server:getIDs')
-end)
-
-RegisterNetEvent('qb-cityhall:client:sendDriverEmail', function(charinfo)
-    SetTimeout(math.random(2500, 4000), function()
-        local gender = Lang:t('email.mr')
-        if PlayerData.charinfo.gender == 1 then
-            gender = Lang:t('email.mrs')
-        end
-        TriggerServerEvent('qb-phone:server:sendNewMail', {
-            sender = Lang:t('email.sender'),
-            subject = Lang:t('email.subject'),
-            message =  Lang:t('email.message', {gender = gender, lastname = charinfo.lastname, firstname = charinfo.firstname, phone = charinfo.phone}),
-            button = {}
-        })
-    end)
 end)
 
 AddEventHandler('onResourceStop', function(resource)
@@ -241,95 +118,8 @@ AddEventHandler('onResourceStop', function(resource)
     deletePeds()
 end)
 
--- NUI Callbacks
-
-RegisterNUICallback('close', function(_, cb)
-    setCityhallPageState(false, false)
-    if not Config.UseTarget and inRangeCityhall then exports['ps-ui']:DisplayText('[E] Open Cityhall') end -- Reopen interaction when you're still inside the zone
-    cb('ok')
-end)
-
-RegisterNUICallback('requestId', function(id, cb)
-    local license = Config.Cityhalls[closestCityhall].licenses[id.type]
-    if inRangeCityhall and license and id.cost == license.cost then
-        TriggerServerEvent('qb-cityhall:server:requestId', id.type, closestCityhall)
-        QBCore.Functions.Notify(('You have received your %s for $%s'):format(license.label, id.cost), 'success', 3500)
-    else
-        QBCore.Functions.Notify(Lang:t('error.not_in_range'), 'error')
-    end
-    cb('ok')
-end)
-
-RegisterNUICallback('requestLicenses', function(_, cb)
-    local licensesMeta = PlayerData.metadata["licences"]
-    local availableLicenses = table_clone(Config.Cityhalls[closestCityhall].licenses)
-    for license, data in pairs(availableLicenses) do
-        if data.metadata and not licensesMeta[data.metadata] then
-            availableLicenses[license] = nil
-        end
-    end
-    cb(availableLicenses)
-end)
-
-RegisterNUICallback('applyJob', function(job, cb)
-    if inRangeCityhall then
-        TriggerServerEvent('qb-cityhall:server:ApplyJob', job, Config.Cityhalls[closestCityhall].coords)
-    else
-        QBCore.Functions.Notify(Lang:t('error.not_in_range'), 'error')
-    end
-    cb('ok')
-end)
-
 -- Threads
-
-CreateThread(function()
-    while true do
-        if isLoggedIn then
-            playerPed = PlayerPedId()
-            playerCoords = GetEntityCoords(playerPed)
-            closestCityhall = getClosestHall()
-            closestDrivingSchool = getClosestSchool()
-        end
-        Wait(1000)
-    end
-end)
-
 CreateThread(function()
     initBlips()
     spawnPeds()
-    QBCore.Functions.TriggerCallback('qb-cityhall:server:receiveJobs', function(result)
-        SendNUIMessage({
-            action = 'setJobs',
-            jobs = result
-        })
-    end)
-    if not Config.UseTarget then
-        while true do
-            local sleep = 1000
-            if isLoggedIn and closestCityhall and closestDrivingSchool then
-                if inRangeCityhall then
-                    if not inCityhallPage then
-                        sleep = 0
-                        if IsControlJustPressed(0, 38) then
-                            setCityhallPageState(true, true)
-                            exports['qb-core']:KeyPressed()
-                            Wait(500)
-                            exports['ps-ui']:HideText()
-                            sleep = 1000
-                        end
-                    end
-                elseif inRangeDrivingSchool then
-                    sleep = 0
-                    if IsControlJustPressed(0, 38) then
-                        TriggerServerEvent('qb-cityhall:server:sendDriverTest', Config.DrivingSchools[closestDrivingSchool].instructors)
-                        sleep = 5000
-                        exports['qb-core']:KeyPressed()
-                        Wait(500)
-                        exports['ps-ui']:HideText()
-                    end
-                end
-            end
-            Wait(sleep)
-        end
-    end
 end)
--- config.lua
+++ config.lua
@@ -3,59 +3,33 @@ Config = Config or {}
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
 Config.Cityhalls = {
-    { -- Cityhall 1
-        coords = vec3(-265.0, -963.6, 31.2),
-        showBlip = true,
-        blipData = {
-            sprite = 487,
-            display = 4,
-            scale = 0.65,
-            colour = 0,
-            title = "City Services"
-        },
-        licenses = {
-            ["id_card"] = {
-                label = "ID Card",
-                cost = 50,
-            },
-            ["driver_license"] = {
-                label = "Driver License",
-                cost = 50,
-                metadata = "driver"
-            },
-            ["weaponlicense"] = {
-                label = "Weapon License",
-                cost = 50,
-                metadata = "weapon"
-            },
-        }
+    coords = vector3(-543.94, -196.79, 38.23),
+    showBlip = true,
+    blipData = {
+        sprite = 487,
+        display = 4,
+        scale = 1.0,
+        colour = 0,
+        title = "市政府"
     },
-}
-
-Config.DrivingSchools = {
-    { -- Driving School 1
-        coords = vec3(240.3, -1379.89, 33.74),
-        showBlip = true,
-        blipData = {
-            sprite = 225,
-            display = 4,
-            scale = 0.65,
-            colour = 3,
-            title = "Driving School"
+    licenses = {
+        ["id_card"] = {
+            label = "身分證",
+            cost = 1250,
         },
-        instructors = {
-            "DJD56142",
-            "DXT09752",
-            "SRI85140",
-        }
-    },
+        ["driver_license"] = {
+            label = "駕駛執照",
+            cost = 1250,
+            metadata = "driver"
+        },
+    }
 }
 
 Config.Peds = {
     -- Cityhall Ped
     {
-        model = 'a_m_m_hasjew_01',
-        coords = vec4(-262.79, -964.18, 30.22, 181.71),
+        model = 's_m_m_fiboffice_02',
+        coords = vector4(-542.53, -197.14, 38.24, 74.52),
         scenario = 'WORLD_HUMAN_STAND_MOBILE',
         cityhall = true,
         zoneOptions = { -- Used for when UseTarget is false
@@ -64,15 +38,4 @@ Config.Peds = {
             debugPoly = false
         }
     },
-    -- Driving School Ped
-    {
-        model = 'a_m_m_eastsa_02',
-        coords = vec4(240.91, -1379.2, 32.74, 138.96),
-        scenario = 'WORLD_HUMAN_STAND_MOBILE',
-        drivingschool = true,
-        zoneOptions = { -- Used for when UseTarget is false
-            length = 3.0,
-            width = 3.0
-        }
-    }
 }
